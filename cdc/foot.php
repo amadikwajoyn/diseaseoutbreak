1d33b5a6120ff8ef62e70938d0e2476e2f167831
treasurechristain <amadikwajoyn@gmail.com>
2020-01-08 02:36:49 -0800

    first commit

---
+++ cdc/foot.php
@@ -0,0 +1,84 @@
+
+	<!--footer-->
+	<div class="footer">
+		<div class="container">
+			<h3 class="title">GET IN <span>TOUCH</span></h3>
+			<div class="footer-grids">
+				<div class="col-md-6 footer-left">
+					<h5><a href="#">Ready to Report an Outbreak?</a></h5>
+					<div class="more">
+						<a href="report.php" class="effect6">Report Now!</a>
+					</div>
+				</div>
+				<div class="col-md-6 footer-right">
+					<div class="address">
+						<div class="col-xs-2 contact-grdl">
+							<span class="glyphicon glyphicon-phone" aria-hidden="true"></span>
+						</div>
+						<div class="col-xs-10 contact-grdr">
+							<p>+234 701 404 8002</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="address">
+						<div class="col-xs-2 contact-grdl">
+							<span class="glyphicon glyphicon-send" aria-hidden="true"></span>
+						</div>
+						<div class="col-xs-10 contact-grdr">
+							<p>CDC Office, JABI, ABUJA, NIGERIA.</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="address">
+						<div class="col-xs-2 contact-grdl">
+							<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+						</div>
+						<div class="col-xs-10 contact-grdr">
+							<p><a href="mailto:example@mail.com">mail@example.com</a></p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!--//footer-->
+	<!--copy-right-->
+	<div class="copy-right">
+		<div class="container">
+			<ul>
+				<li><a href="#"></a></li>
+				<li><a href="#" class="gg"></a></li>
+				<li><a href="#" class="twt"></a></li>
+				<li><a href="#" class="be"></a></li>
+			</ul>
+			<div class="clearfix"> </div>
+			<p>© 2018 CDC . All rights reserved | Design by <a href="">CDC</a></p>	
+		</div>
+	</div>
+	<!--//copy-right-->
+	<!--smooth-scrolling-of-move-up-->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+			var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+			};
+			*/
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	<!--//smooth-scrolling-of-move-up-->
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
